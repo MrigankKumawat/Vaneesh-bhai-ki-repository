cfb30aefb486c4cd2ec8852343be8ca29bf28ad4
MrigankKumawat <mrigankkumawat@gmail.com>
2024-12-02 23:43:00 +0530

    Code pushed

---
+++ Vaneesh.c
@@ -0,0 +1,160 @@
+#include<stdio.h>
+
+// ARRAYS
+
+// Question 1- To input N numbers in an array and find their sum and average ?
+
+// int main(){
+//     int n;
+//     printf("Enter how many numbers you want to add:");
+//     scanf("%d",&n);
+//     int arr[n];
+//     printf("Enter Numbers-:\n");
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+
+//     // For Sum
+
+//     int sum=0;
+//     for(int x=0;x<n;x++){
+//         sum=sum+arr[x];
+//     }
+//     printf("Sum:%d\n",sum);
+
+//     // For Average
+
+//     int avg=sum/n;
+//     printf("Average:%d",avg);
+// }
+
+// Question 2- To input N numbers in an array and display odd numbers and numbers at odd indices ?
+
+// int main(){
+//     int n;
+//     printf("Enter how many numbers you want to add:");
+//     scanf("%d",&n);
+//     int arr[n];
+//     printf("Enter numbers-:\n");
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+
+//     // To display odd numbers
+
+//     printf("Odd numbers are:");
+//     for(int x=0;x<n;x++){
+//         if(arr[x]%2!=0){
+//             printf(" %d",arr[x]);
+//         }
+//     }
+
+//     // To display Odd indices
+
+//     printf("\nValues on Odd indices:");
+//     for(int i=0;i<n;i++){
+//         if(i%2!=0){
+//             printf(" %d",arr[i]);
+//         }
+//     }
+// }
+
+// Question 3- To input N numbers in an array and display even numbers and numbers at even indices ?
+
+// int main(){
+//     int n;
+//     printf("Enter how many numbers to add:");
+//     scanf("%d",&n);
+//     int arr[n];
+//     printf("Enter numbers-:\n");
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+
+//     // To display Even numbers
+
+//     printf("Even numbers:");
+//     for(int x=0;x<n;x++){
+//         if(arr[x]%2==0){
+//             printf(" %d",arr[x]);
+//         }
+//     }
+
+//     // To display Even indices
+
+//     printf("\nValues on Even indices:");
+//     for(int i=0;i<n;i++){
+//         if(i%2==0){
+//             printf(" %d",arr[i]);
+//         }
+//     }
+// }
+
+// Question 4- To input N numbers in an array and display maximum and minimum values ?
+
+// int main(){
+//     int n;
+//     printf("Enter how many values you want to add:");
+//     scanf("%d",&n);
+//     int arr[n];
+//     printf("Enter numbers-:\n");
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+
+//     // To display Maximum number 
+
+//     int max=arr[1];
+//     printf("Maximum Number:");
+//     for(int x=0;x<n;x++){
+//         if(arr[x]>max){
+//             max=arr[x];
+//         }
+//     }
+//     printf("%d",max);
+
+//     // To display Minimum number
+
+//     int min=arr[0];
+//     printf("\nMinimum Number:");
+//     for(int y=0;y<n;y++){
+//         if(arr[y]<min){
+//             min=arr[y];
+//         }
+//     }
+//     printf("%d",min);
+// }
+
+// Question 5- To input N numbers in an array and print factorial of each number ?
+
+int main(){
+    int n;
+    printf("Enter how many values you want to add:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter numbers-:\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int fact=1;
+    for(int x=0;x>)
+
+}
+
+// int main(){
+//     int num;
+//     printf("Enter:");
+//     scanf("%d",&num);
+//     int fact=1;
+//     for(int i=num;i>0;i--){
+//         fact*=i;
+//     }
+//     printf("%d",fact);
+// }
+
+
+
+
+
+
+
